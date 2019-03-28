d71268f215a9c66222921ca7b4214607ddea518a
LG <gksrudehd123@naver.com>
2019-03-28 22:23:53 +0900

    Algorithm_10950,10952

---
+++ src/Problem_10950/Main.java
@@ -0,0 +1,22 @@
+package Problem_10950;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int test = scanner.nextInt();
+		
+		for(int i=0;i<test;i++) {
+			int num1 = scanner.nextInt();
+			int num2 = scanner.nextInt();
+			
+			System.out.println(num1+num2);
+			
+		}
+
+	}
+
+}
