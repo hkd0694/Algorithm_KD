7cd1e124ba5da6254500965fd2d34dc1b3778433
LG <gksrudehd123@naver.com>
2019-03-28 14:58:29 +0900

    Algorithm_2748

---
+++ src/Problem_2748/Main.java
@@ -0,0 +1,34 @@
+package Problem_2748;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		long test = scanner.nextInt();
+		
+		long F0 = 0;
+		long F1 = 1;
+		long F = 0;
+		int count = 2;
+		
+		if(test >= 0 && test <=90) {
+			if(test == 0) System.out.println(0);
+			else if(test == 1) System.out.println(1);
+			else if(test == 2) System.out.println(1);
+			else {
+				do {
+					F = F0+F1;
+					F0 = F1;
+					F1 = F;
+					count++;
+				}while(count != test);
+				System.out.println(F0+F1);
+			}
+		}
+
+	}
+}
