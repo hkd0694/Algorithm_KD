e22afcc1e7026c048d3b4427d1c0a95d4f20324e
LG <gksrudehd123@naver.com>
2019-03-30 13:47:04 +0900

    Algorithm_10872

---
+++ src/Problem_10872/Main.java
@@ -0,0 +1,25 @@
+package Problem_10872;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int fact = scanner.nextInt();
+		
+		int result = 1;
+		
+		for(int i = fact; i>=1;i--) {
+			result *= i;
+		}
+		
+		System.out.println(result);
+		
+		
+
+	}
+
+}
