c996e82121290d67bec3a0c28caecfc17aa66161
LG <gksrudehd123@naver.com>
2019-04-04 09:03:36 +0900

    Algorithm_1874 수정

--- src/Problem_10828/Main.java
+++ src/Problem_10828/Main.java
@@ -50,5 +50,4 @@ public class Main {
 		bw.flush();
 		bw.close();
 	}
-
 }
--- src/Problem_1874/Main.java
+++ src/Problem_1874/Main.java
@@ -2,106 +2,46 @@ package Problem_1874;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class Main {//죤나 쉽게 푸네...
+public class Main {
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        int i, temp, max = 0, top = 0;
-        int stack[] = new int[n];
-        
-        while (n-- > 0) {
-            temp = Integer.parseInt(br.readLine());
-            
-            if (temp > max) {
-                // 스택에 값이 없는 상태
-                for (i = max+1; i <= temp; i++) {
-                    stack[top++] = i;
-                    sb.append("+\n");
-                }
-                max = temp;
-            }else if (stack[top - 1] != temp) {
-                System.out.print("NO");
-                return;
-            }
- 
-            top--;
-            sb.append("-\n");
-        
-        }// end while
-        
-        System.out.println(sb);
+        int test = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+        int index = 1;
+        int i=0;
+        for(;i<test;i++) {
+        	int num = Integer.parseInt(br.readLine());
+        	int start = index;
+        	if(stack.isEmpty()) {
+        		for(int j=start;j<=num;j++) {
+        			stack.push(j);
+        			sb.append("+\n");
+        			index++;
+        		}
+        		stack.pop();
+        		sb.append("-\n");
+        	} else {
+        		if(stack.peek() == -1) continue;
+        		else if(stack.peek() == num) {
+        			stack.pop();
+        			sb.append("-\n");
+        		} else if(stack.peek() > num) stack.push(-1);
+        		 else {
+        			for(int z=start;z<=num;z++) {
+        				stack.push(z);
+        				sb.append("+\n");
+        				index++;
+        			}
+        			stack.pop();
+        			sb.append("-\n");
+        		}
+        	}
+        }
+        if(stack.isEmpty()) System.out.println(sb);
+        else System.out.println("NO");
     }
-	
-	/*public static void main(String[] args) {//제대로 출력되는데 런타임 에러 뜬다... 이유 모르겠음
-		
-		Scanner scanner = new Scanner(System.in);
-		int size = -1;
-		int alpa = 0;
-		int topNumber = 0;
-		int test = scanner.nextInt();
-		char a[] = new char[300000];
-		int stack[] = new int[test];
-		for(int i=0;i<stack.length;i++) {
-			int num = scanner.nextInt();
-			if(i==0) {
-				topNumber = num+1; //i=1이고 num이 클경우 topNumber부터 찾아야함
-				if(num == 1) {
-					a[alpa] = '+';
-					alpa++;
-					a[alpa] = '-';
-					alpa++;
-					size++;
-					stack[size] = 1;
-					continue;
-				}
-				for(int j=1;j<=num;j++) {
-					size++;
-					stack[size] = j;
-					a[alpa] = '+';
-					alpa++;
-				}
-				stack[size] = 0;
-				size--;
-				a[alpa] = '-';
-				alpa++;
-			} else {
-				if(stack[size] == num) {
-					a[alpa]='-';
-					alpa++;
-					stack[size] = 0;
-					size--;
-				}
-				else if(stack[size] > num) { //스택 오류
-					a[0] = 'p';
-					a[alpa] = 'p';
-					alpa++;
-				} else {
-					for(int k=topNumber;k<=num;k++) {
-						size++;
-						stack[size] = k;
-						a[alpa]='+';
-						alpa++;
-					}
-					stack[size] = 0;
-					size--;
-					a[alpa]='-';
-					alpa++;
-					topNumber = num+1;
-				}
-			}
-		}
-		
-		if(a[0] != 'p') {
-			for(int l=0;l<alpa;l++) {
-				System.out.println(a[l]);
-			}
-		} else {
-			System.out.println("NO");
-		}
-	}*/
 }
