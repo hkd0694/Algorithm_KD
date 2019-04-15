86b243cf61c22f8550d7d345a7b1ddfd7e8c48d4
LG <gksrudehd123@naver.com>
2019-04-15 20:59:35 +0900

    [A-B : 두 수를 입력 받고 뺄셈을 한 결과를 출력하는 문제]

--- BaekJoon/PB_1000/Main.java
+++ BaekJoon/PB_1000/Main.java
@@ -1,10 +1,14 @@
 package PB_1000;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		System.out.println("asdasd");
+	public static void main(String[] args) throws IOException {
+		int x = System.in.read()-'0';
+		System.in.read();
+		int y = System.in.read()-'0';		
+		System.out.println(x+y);
 
 	}
 }
