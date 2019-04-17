b386c8ef465faba641d9f8f49ad884ab17f9ec2b
LG <gksrudehd123@naver.com>
2019-04-17 16:17:34 +0900

    [설탕 배달 : 나누기, 나머지 연산을 이용하는 문제]

---
+++ BaekJoon/PB_2839/Main.java
@@ -0,0 +1,29 @@
+package PB_2839;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		
+		while(n>0) {
+			if(n%5==0) {
+				cnt+=n/5;
+				break;
+			}
+		}
+		if(n<0) System.out.println(-1);
+		else System.out.println(cnt);
+		
+		br.close();
+	}
+
+}
