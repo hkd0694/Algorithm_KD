80676ddd496ed0ef0af357f0d72a617104228d7a
LG <gksrudehd123@naver.com>
2019-04-17 17:18:15 +0900

    [기찍 N : N부터 1까지 숫자를 한 줄에 하나씩 출력하는 문제]

---
+++ BaekJoon/PB_2742/Main.java
@@ -0,0 +1,23 @@
+package PB_2742;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		if(num <= 100000) {
+			for(int i=num;i>=1;i--) System.out.println(i);
+		}
+		
+		br.close();
+
+	}
+
+}
