19016efc02320b0453f227ec3bbfbe8166b228ae
LG <gksrudehd123@naver.com>
2019-04-23 20:38:46 +0900

    [RGB거리 : i번째 집을 각각의 색으로 칠할 때, 1-i번째 집을 모두 칠하는 최소 비용을 출력하는 문제]

---
+++ BaekJoon/PB_1149/Main.java
@@ -0,0 +1,31 @@
+package PB_1149;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	   public static void main(String[] args) throws NumberFormatException, IOException {
+	       
+	       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	       int n = Integer.parseInt(br.readLine());
+	       StringTokenizer st;
+	       
+	       int arr[][] = new int[n][3];
+	       
+	       for (int i = 0; i < n; i++) {
+	    	   st = new StringTokenizer(br.readLine());
+	          for (int j = 0; j < 3; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+	       }
+	       
+	       for (int i = 1; i < n; i++) {
+	          arr[i][0] = arr[i][0] + Math.min(arr[i - 1][1], arr[i - 1][2]);
+	          arr[i][1] = arr[i][1] + Math.min(arr[i - 1][0], arr[i - 1][2]);
+	          arr[i][2] = arr[i][2] + Math.min(arr[i - 1][0], arr[i - 1][1]);
+	       }
+	       System.out.println(Math.min(Math.min(arr[n-1][0], arr[n-1][1]), arr[n-1][2]));
+	       br.close();
+	 }
+}
