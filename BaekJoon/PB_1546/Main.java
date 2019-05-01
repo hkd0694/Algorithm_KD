53eb7bf19e8932fc7db7af86f1db42dda0922df7
LG <gksrudehd123@naver.com>
2019-05-02 08:17:50 +0900

    [평균 : 최대값을 찾아, 그 값으로 다른 값들을 바꾼 후 평균을 구하는 문제]

---
+++ BaekJoon/PB_1546/Main.java
@@ -0,0 +1,32 @@
+package PB_1546;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//C로 풀었음!!
+		/*int count,i;
+		double *grade;
+		double max = 0,sum = 0;
+
+		scanf("%d",&count);
+		
+		grade = (double*)malloc(sizeof(double)*count);
+
+		for (i = 0; i < count; i++) {
+			scanf("%lf",&grade[i]);
+			if (max < grade[i])
+				max = grade[i];
+		}
+
+		for (i = 0; i < count; i++) {
+			grade[i] = grade[i] / max * 100;
+			sum += grade[i];
+		}
+
+		printf("%.2f",sum/count);
+
+		return 0;*/
+
+	}
+
+}
