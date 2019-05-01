2c394187945277cdfc9cc5dd3f5267d7e8eff365
LG <gksrudehd123@naver.com>
2019-05-02 08:22:33 +0900

    [더하기 사이클 : 조건을 만족할 때까지 계속 더하는 문제]

--- BaekJoon/PB_1110/Main.java
+++ BaekJoon/PB_1110/Main.java
@@ -1,4 +1,4 @@
-package Problem_1110;
+package PB_1110;
 
 import java.util.Scanner;
 
@@ -21,43 +21,13 @@ public class Main {
 				else {
 					result = num1 / 10 + num1 % 10;
 				}
-
 				num1 = (num1 % 10)*10 + (result % 10);
-
 				count++;
 			} while (num != num1);
 
 			System.out.println(count);
 		}
+
 	}
 
 }
-
-/*#include <stdio.h>
-
-int main() {
-
-	int num,count=0;
-	int num1;
-	int result=0;
-
-	scanf("%d",&num);
-	if (num >= 0 && num < 100) {
-		num1 = num;
-		do {
-			if (num1 < 10) {
-				result = num1 % 10;
-			}
-			else {
-				result = num1 / 10 + num1 % 10;
-			}
-
-			num1 = (num1 % 10)*10 + (result % 10);
-
-			count++;
-		} while (num != num1);
-
-		printf("%d",count);
-	}
-	return 0;
-}*/
