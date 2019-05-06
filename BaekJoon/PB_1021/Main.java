b2d75a173a0b18f03d3c09c2cac457afc4a54af5
LG <gksrudehd123@naver.com>
2019-05-06 11:38:33 +0900

    [회전하는 큐 : 덱을 활용하여 풀 수있는 문제 (수정) ]

--- BaekJoon/PB_1021/Main.java
+++ BaekJoon/PB_1021/Main.java
@@ -18,15 +18,16 @@ public class Main {
 		int test = Integer.parseInt(st.nextToken());
 		Deque<Integer> de = new ArrayDeque<>();
 		
-		for(int i=1;i<=number;i++) de.add(i);
+		for(int i=1;i<=number;i++) de.add(i); //queue에 값을 모두 집어넣는다.
 
 		int count = 0;
 		st = new StringTokenizer(br.readLine());
 		for(int j=0;j<test;j++) {
 			int index = Integer.parseInt(st.nextToken());
 			Iterator<Integer> iterator = de.iterator();
-			int Fcount = 0;
+			int Fcount = 0; //2번 연산 count 값 저장할 변수
 			while(iterator.hasNext()) {
+				//iterator를 통하여 queue안에 있는 값들을 앞에서부터 count한다.
 				int a = iterator.next();
 				if(a == index) {
 					break;
@@ -34,15 +35,20 @@ public class Main {
 					Fcount++;
 				}
 			}
-			int Lcount = de.size() - Fcount;
+			int Lcount = de.size() - Fcount;  //3번 연산 count 값 저장할 변수
+			//2번 연산보다 3번연산이 더 클경우
 			if(Fcount <= Lcount) {
+				//2번 연산 과정
 				while(index != de.peekFirst()) {
 					int Fde = de.pollFirst();
 					de.addLast(Fde);
 					count++;
 				}
 				de.pollFirst();
-			} else {
+			} 
+			//2번 연산보다 3번연산이 더 작을 경우
+			else {
+				//3번 연산 과정
 				while(index != de.peekFirst()) {
 					int Lde = de.pollLast();
 					de.addFirst(Lde);
