7287d1498c9b60c8c6e1166de5b4308b63ce2888
LG <gksrudehd123@naver.com>
2019-05-06 11:36:57 +0900

    [링 : 첫번째 링을 한 바퀴 돌렸을 때, 나머지 링이 몇바퀴 도는 지 구하는 문제 (수정)]

--- BaekJoon/PB_3036/Main.java
+++ BaekJoon/PB_3036/Main.java
@@ -10,14 +10,24 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	private static int gcd(int a, int b) {
-		int M1 = a;
-		int N1 = b;
-		while(M1 != 0) {
-			int div = N1%M1;
-			N1=M1;
-			M1=div;
+		int N1 = a; //두개의 수중 작은 수
+		int M1 = b; //두개의 수중 큰 수
+		//N1이 0이 될깨까지 while문을 돈다
+		while(N1 != 0) {
+			//M1을 N1으로 나누어주고 나머지를 div에 저장
+			int div = M1%N1;
+			//M1을 기존의 N1값으로 저장
+			M1=N1;
+			//N1을 위에서 구한 div에 값으로 저장
+			N1=div;
 		}
-		return N1;
+		//ex) 8 12가 들어오면 
+		//N1 M1 -- 8 12
+		//N1 M1 -- 4 8
+		//N1 M1 -- 0 4
+		//N1이 0이되면 while문을 종료시킨다. 
+		//종료되면 저장되어 있는 M1의 값이 두값의 최대공약수가 된다.
+		return M1;
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -27,17 +37,18 @@ public class Main {
 		int test = Integer.parseInt(br.readLine());
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken()); //가장 첫번째로 주어진 수
 		StringBuilder sb = new StringBuilder();
 		for(int i=1;i<test;i++) {
+			//두번째부터 test까지의 수를 N에다가 하나씩 저장
 			int N = Integer.parseInt(st.nextToken());
-			int ok = gcd(N,M);
-			
-			sb.append(M/ok +"/" + N/ok +"\n");
+			//첫번째수와 N과의 최대공약수를 구하는 gcd 함수
+			int gcd = gcd(Math.min(M, N),Math.max(M, N));
+			//M과 N을 최대공약수로 나누어 몫을 StringBuilder에 저장
+			sb.append(M/gcd +"/" + N/gcd +"\n");
 		}
-		
+		//StringBuilder에 있는 것을 출력해준다.
 		bw.write(String.valueOf(sb));
-		
 		bw.flush();
 		bw.close();
 		br.close();
