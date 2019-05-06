045f1261493afd89a313ddf9921f8aa53549558e
LG <gksrudehd123@naver.com>
2019-05-06 11:40:14 +0900

    [팩토리얼 0의 개수 : N!의 뒤에서 처음 0이 아닌 숫자가 나올 때까지의 0의 개수를 구하는 문제 (수정)]

--- BaekJoon/PB_1676/Main.java
+++ BaekJoon/PB_1676/Main.java
@@ -17,17 +17,20 @@ public class Main {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
-		BigInteger result = factorial(num);
-		String asd = String.valueOf(result);
+		BigInteger result = factorial(num); //factorial 의 수를 result 변수안에 넣음.
+		String NumberString = String.valueOf(result);//받은 result을 문자열로 변경
 		
-		int length = asd.length();
+		int length = NumberString.length(); //문자열로 바꾼 숫자의 길이를 구하는 변수
 		int count = 0;
 		
 		while(length != 0) {
-			if(asd.charAt(length-1) == '0') count++;
+			//문자열로 바뀐 NumberString의값의 맨 마지막 인덱스부터 그 값이 0이면 count를 증가시키고,
+			//만약 0이 아닌 숫자가 오면 바로 break를 해줌으로써 while문을 종료시킨다.
+			if(NumberString.charAt(length-1) == '0') count++;
 			else break;
 			length--;
 		}
 		System.out.println(count);
+		br.close();
 	}
 }
