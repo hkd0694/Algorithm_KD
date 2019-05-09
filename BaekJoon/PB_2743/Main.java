bbb9dfd9cb7965096cf06f586d2538b4518db504
LG <gksrudehd123@naver.com>
2019-05-09 21:42:29 +0900

    [단어 길이 재기 : 주어진 문자의 길이를 출력하는 문제]

---
+++ BaekJoon/PB_2743/Main.java
@@ -0,0 +1,20 @@
+package PB_2743;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String len = br.readLine();
+		
+		
+		System.out.println(len.length());
+
+	}
+
+}
